a8625b3d6c63802be2f63f33ce2cfdb6df0aa876
jeanmerced <jean.merced2@upr.edu>
2020-05-22 20:55:18 -0400

    added mproc products functionality

---
+++ ICTS_API_v1/Models/MPROCProduct.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ICTS_API_v1.Models
+{
+    public class MPROCProduct
+    {
+        public string STEPNAME { get; set; }
+
+        public string LOTID { get; set; }
+
+        public string PRODUCTNAME { get; set; }
+
+        public string USEBEFOREDATE { get; set; }
+
+        public int LOT_CREATION_DATE { get; set; }
+
+        public int LOT_TIMEHERESINCE_AGING { get; set; }
+
+        public int COMPONENTQTY { get; set; }
+    }
+}
---
+++ ICTS_API_v1/Services/MPROCProductsService.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using ICTS_API_v1.Models;
+using Microsoft.AspNetCore.Hosting;
+
+
+namespace ICTS_API_v1.Services
+{
+    public class MPROCProductsService
+    {
+        public MPROCProductsService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        public string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "Data", "MPROCProducts.json"); }
+        }
+
+        public IEnumerable<MPROCProduct> GetMPROCProducts()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<MPROCProduct[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+
+            }
+        }
+    }
+}
